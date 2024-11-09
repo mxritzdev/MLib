3b95089e5938b2efa5f1a52d48770eba80accccc
Moritz <101179677+mxritzdev@users.noreply.github.com>
2024-11-09 12:33:37 +0100

    added filehelper

---
+++ MLib.Net/Helpers/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace MLib.Helpers;
+
+public static class FileHelper
+{
+    public static void EnsureFileExists(string filePath, string? initialContent = null)
+    {
+        string path = Path.GetFullPath(filePath);
+        
+        // can throw ArgumentExeption if the path is not fully qualified of the path contains invalid characters
+        
+        string? directory = Path.GetDirectoryName(path);
+        
+        // can throw PathTooLongException if the path is too long
+        
+        if (directory != null) 
+            Directory.CreateDirectory(directory);
+
+        if(!File.Exists(path))
+            File.WriteAllText(path, initialContent);
+
+    }
+}
--- MLib.Net/Helpers/TestHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MLib.Helpers;
-
-public class TestHelper
-{
-    public static void Test(string test)
-    {
-        Console.WriteLine(test);
-    }
-}
